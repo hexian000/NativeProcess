68407497563625c1190654501eca8eb7a0f430d2
hexian000 <he.xian@163.com>
2020-06-05 02:02:05 +0800

    update

--- app/src/main/cpp/read_pid.c
+++ app/src/main/cpp/read_pid.c
@@ -10,33 +10,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-static inline char *find_name(char *stat_line, char *buf, size_t bufsize) {
-  stat_line = strchr(stat_line, '(');
-  if (stat_line == NULL) {
-    return NULL;
-  }
-  size_t count = 0;
-  int b = 1;
-  for (char c = *++stat_line; c && count < bufsize; c = *++stat_line) {
-    switch (c) {
-      case '(':b++;
-        break;
-      case ')':b--;
-        if (b == 0) {
-          buf[count] = 0;
-          return stat_line + 1;
-        }
-        break;
-      default:
-        if (c < 0 || !isprint(c)) {
-          c = '?';
-        }
-        buf[count++] = c;
-    }
-  }
-  return NULL;
-}
-
 bool read_pid(int pid, struct procinfo *info, char *name, size_t name_len) {
   info->pid = pid;
   int fd;
@@ -52,29 +25,47 @@ bool read_pid(int pid, struct procinfo *info, char *name, size_t name_len) {
     return false;
   }
 
-  ssize_t n = read(fd, buf, sizeof(buf) - 1);
-  if (n < 0) {
+  ssize_t nread = read(fd, buf, sizeof(buf) - 1);
+  if (nread < 0) {
+    close(fd);
     return false;
   }
-  buf[n] = '\0';
-
-  char *p = find_name(buf, name, name_len);
-  if (p == NULL) {
-    return false;
-  }
+  buf[nread] = '\0';
 
   unsigned long utime, stime;
   long rss;
-  if (sscanf(p,
-             " %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu %*d %*d %*d %*d %*d %*d %*u %*u %ld",
+  if (sscanf(buf,
+             "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu %*d %*d %*d %*d %*d %*d %*u %*u %ld",
              &utime,
              &stime,
              &rss) != 3) {
+    close(fd);
     return false;
   }
 
   info->time = utime + stime;
   info->resident = rss;
 
+  close(fd);
+  snprintf(buf, sizeof(buf), "/proc/%d/cmdline", pid);
+  fd = open(buf, O_RDONLY);
+  if (fd == -1) {
+    return false;
+  }
+
+  nread = read(fd, name, name_len - 1);
+  if (nread < 0) {
+    close(fd);
+    return false;
+  }
+  name[nread] = '\0';
+
+  for (size_t i = 0; i < (size_t) nread; i++) {
+    if (!isprint(name[i])) {
+      name[i] = '?';
+    }
+  }
+
+  close(fd);
   return true;
 }
